6fb4c46caef0967eb49144b76f0cd8964dea0627
R. Tyler Croy <tyler@monkeypox.org>
2011-10-15 12:42:39 -0700

    Install the git and github plugin by default in the pristine box
    These are kind of important dependencies so why not

--- pristine/manifests/base.pp
+++ pristine/manifests/base.pp
@@ -5,8 +5,20 @@
 node default {
   include jenkins
 
+  install-jenkins-plugin {
+    "git-plugin" :
+      name => "git",
+      require => Class["jenkins"];
+
+    "github-plugin" :
+      name => "github",
+      require => Class["jenkins"];
+  }
+
   group {
     "puppet" :
       ensure => present;
   }
 }
+
+# vim: ts=2 et sw=2 autoindent
