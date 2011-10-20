d8b098d0be81b37ad6c8b79efb1a46af7ccc5f73
R. Tyler Croy <tyler@monkeypox.org>
2011-10-19 17:51:56 -0700

    Bundle a couple rubies in the default image

--- with-rvm/manifests/base.pp
+++ with-rvm/manifests/base.pp
@@ -15,6 +15,17 @@ node default {
     "jenkins" :
       require => User["jenkins"];
   }
+
+  if $rvm_installed == "true" {
+    rvm_system_ruby {
+      'ruby-1.9.2-p290':
+        ensure => 'present',
+        default_use => false;
+      'ruby-1.8.7-p352':
+        ensure => 'present',
+        default_use => true;
+    }
+  }
 }
 
 # vim: ts=2 et sw=2 autoindent
