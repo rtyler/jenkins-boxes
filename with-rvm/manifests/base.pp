1367627f29df544abdb9fd2b0f83a8982a0ec225
R. Tyler Croy <tyler@monkeypox.org>
2011-10-20 19:57:37 -0700

    Feh

--- with-rvm/manifests/base.pp
+++ with-rvm/manifests/base.pp
@@ -6,6 +6,9 @@ import "rvm"
 node default {
   include rvm::system
 
+  $ruby_18 = "ruby-1.8.7-p352"
+  $ruby_19 = "ruby-1.9.2-p290"
+
   user {
     "jenkins" :
       ensure => present;
@@ -17,15 +20,17 @@ node default {
   }
 
   if $rvm_installed == "true" {
+    notice("Building Rubies..")
     rvm_system_ruby {
-      'ruby-1.9.2-p290':
-        ensure => 'present',
+      "$ruby_19" :
+        ensure => "present",
+        require => Exec["system-rvm"],
         default_use => false;
-      'ruby-1.8.7-p352':
-        ensure => 'present',
+      "$ruby_18" :
+        ensure => "present",
+        require => Exec["system-rvm"],
         default_use => true;
     }
   }
 }
-
 # vim: ts=2 et sw=2 autoindent
